f204af228c7feb916d7fd878791b49658ce7a66d
Ayshashaz <167733549+Ayshashaz@users.noreply.github.com>
2024-05-22 20:05:57 +0400

    Add files via upload

---
+++ PoundstoDollars.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+amount_in_pounds = float(input("Please enter the amount in pounds: "))
+conversion_rate = 1.37
+amount_in_dollars = amount_in_pounds * conversion_rate
+print("£", amount_in_pounds, "are $", amount_in_dollars)
+
+
+# In[ ]:
+
+
+
+
